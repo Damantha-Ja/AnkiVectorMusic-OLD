418b06f062e7bbbfb06e1daf82680e5c3c72dda3
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-12 13:40:50 +0530

    Anki Vector Music

--- str.py
+++ str.py
@@ -3,7 +3,7 @@ import asyncio
 from pyrogram import Client
 
 
-print("Enter your app information from my.telegram.org/apps below.")
+print("Enter your app information from my.telegram.org/apps below.")ල්
 
 
 async def main():
